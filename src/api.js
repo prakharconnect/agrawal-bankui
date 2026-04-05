7783bb4fb610817c70b79c75ff8c395400c9513d
Prakhar <prakharietdavv@gmail.com>
2026-04-05 13:20:02 +0530

    Final token changes

--- src/api.js
+++ src/api.js
@@ -12,4 +12,14 @@ const api = axios.create({
     withCredentials: true 
 });
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token"); // 👈 Auth0 token
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
 export default api;
